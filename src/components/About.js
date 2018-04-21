0a53cf92e4927177a1762c762c162c99cdb95d4f
Jessica Cunha <jessicacunha98@hotmail.com>
2018-04-21 14:11:48 +0100

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About section">
+        <h1 className="about-intro"><strong>EXTRA Cenas</strong> O site que tem cenas extras!</h1>
+        <p className="about-item">Este site ajuda <strong> jovens </strong>a encontrar trabalhos part-time, trabahos em festivais ou voluntariado.</p>
+        </div>
+    );
+  }
+}
+export default About;
