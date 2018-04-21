41492554e3228d8bc139514e0955edc49f1c2687
Jessica Cunha <jessicacunha98@hotmail.com>
2018-04-21 13:06:47 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "App">
+      </div>
+    )
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
