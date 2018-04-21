b865fc4cee04c5461bd28dd4096b79d93f54b9b0
Victor Carvalho <contato@victorcarvalho.pt>
2018-04-21 14:31:49 +0100

    DB info and prettifying index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
+// Libraries
 const fs = require('fs');
-const CONFIG = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf-8'))
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Modules
 const libs = require('./functionals/libs');
 const database = require('./functionals/database');
+const files = libs.files(__dirname + '/controllers');
+const handlers = libs.handlers(files, "../controllers/");
+
+// Config
+const CONFIG = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf-8'))
+const db_url = "mongodb://" + CONFIG.db.username + ':' + CONFIG.db.password + '@' + CONFIG.db.address + ':' + CONFIG.db.port + '/' + CONFIG.db.dbname
+
 var app = express();
-
-var files = libs.files(__dirname + '/controllers');
-var handlers = libs.handlers(files, "../controllers/");
-
-
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -24,7 +28,7 @@ app.post("/api", function(req, res) {
 });
 
 app.listen(CONFIG.port, CONFIG.address, function () {
-    // database.connect("mongodb://localhost:27017/shiftappens", files);
+    database.connect(db_url, files);
 
     console.log("Listening on " + CONFIG.address + ", port " + CONFIG.port)
 });
