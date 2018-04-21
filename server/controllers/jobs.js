efb31435c1ce1bffa476461704191d47f7be96cc
Rafael Paiva <rafael.neves1infor2@gmail.com>
2018-04-21 16:54:13 +0100

    database

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -1,5 +1,11 @@
 // Job.js
+
+var db = require("../functionals/database");
+
 exports.add = function(req, res) {
     console.log(req.body.data);
-    res.send("Heyo");
+    db.libs.jobs.insert(req.body.data, {w:1}, function(err) {
+        if (err) res.send(err);
+        else res.send("Heyo");
+    });    
 }
--- server/functionals/database.js
+++ server/functionals/database.js
@@ -1,10 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
 
-exports.libs = {}
+var libs = {};
 
 exports.connect = function(url, req_libs) {
     MongoClient.connect(url, function(err, db) {
         if (err) return console.log(err);
-        for (lib in req_libs) libs[lib] = db.collection(lib);
+        for (lib of req_libs) libs[lib] = db.collection(lib);
     });
 }
+
+exports.libs = libs;
